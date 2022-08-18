962a47b0386df1d3e51e926b5346b5444df0ac1c
Antonio Obregon <aobregonmx@gmail.com>
2022-08-18 03:21:56 -0500

    Configuracion a la red de Rinkeby usando dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const RINKEBY_RPC_URL = process.env.INFURA;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks:{
+    rinkeby: {
+      url:`https://rinkeby.infura.io/v3/${RINKEBY_RPC_URL}`,
+      accounts:[PRIVATE_KEY]
+    }
+  }
 };

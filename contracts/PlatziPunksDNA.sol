e6cf1b51dd6607f0eb39921904171cbca2c5e4b0
Antonio Obregon <aobregonmx@gmail.com>
2022-09-07 02:34:28 -0500

    Smart Contracts Ended

--- contracts/PlatziPunksDNA.sol
+++ contracts/PlatziPunksDNA.sol
@@ -200,7 +200,18 @@ contract PlatziPunksDNA {
         "ShortHairTheCaesarSidePart"
     ];
 
-    // TODO: Calculate DNA
+    // This pseudo random function is determistic and should not be used on production
+    function deterministicPseudoRandomDNA(uint256 _tokenId, address _minter)
+        public
+        pure
+        returns (uint256)
+    {
+        uint256 combinedParams = _tokenId + uint160(_minter);
+        bytes memory encodedParams = abi.encodePacked(combinedParams);
+        bytes32 hashedParams = keccak256(encodedParams);
+
+        return uint256(hashedParams);
+    }
 
     // Get attributes
     uint8 constant ADN_SECTION_SIZE = 2;
@@ -212,7 +223,8 @@ contract PlatziPunksDNA {
         returns (uint8)
     {
         return uint8(
-            (_dna %(1*10**(_rightDiscard+ADN_SECTION_SIZE))) / (1*10**_rightDiscard)
+            (_dna % (1*10** (_rightDiscard+ADN_SECTION_SIZE) ) ) / 
+            (1*10**_rightDiscard)
             );   
     }
     
